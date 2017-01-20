bdb6273179a16bf937fba4f4c1a4ed138bf70a09
Subhendu Aich <subhendu.aich@grandrounds.com>
2017-01-20 06:35:10 -0800

    full data loaded

--- src/main/java/com/google/loaddata/LoadPufData.java
+++ src/main/java/com/google/loaddata/LoadPufData.java
@@ -26,51 +26,50 @@ import java.util.concurrent.TimeoutException;
 import java.nio.file.Path;
 import java.nio.channels.Channels;
 import java.nio.file.FileSystems;
-
+import java.util.Date;
 public class LoadPufData
 {
 
 	public static void main( String[] args ) throws Exception
 	{
 		
-		//Parse the first line
+		
 		String firstLine = "";
 		String DATASET_NAME = "my_new_dataset";
 		String TABLE_NAME = "raw_puf_data";
-		String FILE_PATH = "//Users//subhenduaich//data//Medicare_Provider_Util_Payment_PUF_CY2014.txt.sample";
-		/**
+		String HEADER_FILE_PATH = "//Users//saich//data//header.txt";		
+		String DATA_FILE_PATH = "//Users//saich//data//Medicare_Provider_Util_Payment_PUF_CY2014.txt.gz";
+		
+		//Parse the header file 
 		try {
-			BufferedReader buffer = new BufferedReader(new FileReader(FILE_PATH));
+			BufferedReader buffer = new BufferedReader(new FileReader(HEADER_FILE_PATH));
 			firstLine = buffer.readLine();
 			buffer.close();
 			System.out.println(firstLine);
 		} catch (IOException ex) {
-			// something bad happened, deal with it.
 			ex.printStackTrace();
 		}
-		//Prepares the schema definition 
+		//Prepares a bigquery table schema from the header data 
 		ArrayList<Field>  fields_list = new ArrayList<Field>();
 		for(String field: firstLine.split("\t")) {
-			if(field.endsWith("_cnt") || field.endsWith("_amt")) 
-				fields_list.add(Field.of(field, Field.Type.integer()));
-			else
+			if(field.endsWith("_cnt") || field.endsWith("_amt"))
+				fields_list.add(Field.of(field, Field.Type.floatingPoint()));
+			else 
 				fields_list.add(Field.of(field, Field.Type.string()));
 		}
 		//create table schema in bigquery dataset
 		Schema schema = Schema.newBuilder().setFields(fields_list).build();
-		//create table 
 		TableId tableId = TableId.of(DATASET_NAME, TABLE_NAME);
 		TableDefinition tableDefinition = StandardTableDefinition.of(schema);
 		TableInfo tableInfo = TableInfo.newBuilder(tableId, tableDefinition).build();
-		// Instantiates a client
 	    BigQuery bigquery = BigQueryOptions.getDefaultInstance().getService();
 		Table table = bigquery.create(tableInfo);
-		**/
-		// load data
-		System.out.println(writeFileToTable(DATASET_NAME, TABLE_NAME, FileSystems.getDefault().getPath(FILE_PATH)));
+		
+		// loads data in the new table 
+		System.out.println(writeFileToTable(DATASET_NAME, TABLE_NAME, FileSystems.getDefault().getPath(DATA_FILE_PATH)));
 	}
 
-	public static long writeFileToTable(String datasetName, String tableName, Path csvPath)
+	public static int writeFileToTable(String datasetName, String tableName, Path csvPath)
       throws IOException, InterruptedException, TimeoutException {
 	    // [START writeFileToTable]
 	    TableId tableId = TableId.of(datasetName, tableName);
@@ -79,25 +78,30 @@ public class LoadPufData
 	        WriteChannelConfiguration.newBuilder(tableId)
 	            .setFormatOptions(csv_options)
 	            .build();
+	        System.out.println(writeChannelConfiguration);
 
-	    BigQuery bigquery = BigQueryOptions.getDefaultInstance().getService();
-	    TableDataWriteChannel writer = bigquery.writer(writeChannelConfiguration);
 	    
-	    // Write data to writer
 	    try {
+	    	BigQuery bigquery = BigQueryOptions.getDefaultInstance().getService();
+		    TableDataWriteChannel writer = bigquery.writer(writeChannelConfiguration);
+		    System.out.println(writer);
+	    	// Write data to writer
 	    	OutputStream stream = Channels.newOutputStream(writer);
-	    	//Files.copy(csvPath, stream);
-	    	System.out.println(Files.copy(csvPath, stream));
+	    	long start = System.currentTimeMillis();
+	    	System.out.println("Bytes copied: " + Files.copy(csvPath, stream));
+	    	stream.close();
+	    	writer.close();
+	    	long stop = System.currentTimeMillis();
+	    	System.out.println("Copy Time taken: " + (stop -start)/1000 + "s");
+	    	// access the load job
+		    Job job = writer.getJob();
+		    System.out.println(job);
+		    job = job.waitFor();
+		    System.out.println(job);
+		    return 1;
 	    }catch(Exception e) {
-
 	    	e.printStackTrace();
-	    }
-	    
-	    // Get load job
-	    Job job = writer.getJob();
-	    job = job.waitFor();
-	    LoadStatistics stats = job.getStatistics();
-	    return stats.getOutputRows();
-	    // [END writeFileToTable]
+	    	return -1;
+	    } 
   }
 }
--- src/main/script/download.sh
+++ src/main/script/download.sh
@@ -5,4 +5,5 @@ mkdir ~/data
 wget http://download.cms.gov/Research-Statistics-Data-and-Systems/Statistics-Trends-and-Reports/Medicare-Provider-Charge-Data/Downloads/Medicare_Provider_Util_Payment_PUF_CY2014.zip
 mv Medicare_Provider_Util_Payment_PUF_CY2014.zip ~/data/
 unzip ~/data/Medicare_Provider_Util_Payment_PUF_CY2014.zip -d ~/data/
-rm ~/data/Medicare_Provider_Util_Payment_PUF_CY2014.zip  
+rm ~/data/Medicare_Provider_Util_Payment_PUF_CY2014.zip
+head -1 ~/data/*.txt > ~/data/header.txt  
